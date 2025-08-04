7dd97625b75509abda31844d5c5118cc476b276d
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-04 09:57:13 +0530

    Add files via upload

--- array.js
+++ array.js
@@ -1,90 +1,110 @@
-let arr = [23, 42, 42, "anikta"]
-let arr2 = ["deepak", "singh", "bhaisora"]
-console.log(arr);
-console.log(arr2);
+// let arr = [23, 42, 42, "anikta"]
+// let arr2 = ["deepak", "singh", "bhaisora"]
+// console.log(arr);
+// console.log(arr2);
 
-arr.push(3434)
-console.log(arr);
-let arr3 = arr + arr2;
-console.log(arr3);
-console.log(...arr, ...arr2);
+// arr.push(3434)
+// console.log(arr);
+// let arr3 = arr + arr2;
+// console.log(arr3);
+// console.log(...arr, ...arr2);
 
-arr3.pop([4])
-console.log(arr3);
+// arr3.pop([4])
+// console.log(arr3);
 
-arr3.pop(23)
-console.log(arr3);
+// arr3.pop(23)
+// console.log(arr3);
 
-myarr = [23, 43, 34, 54]
-console.log(myarr);
+// myarr = [23, 43, 34, 54]
+// console.log(myarr);
 
-myarr.push(545)
-console.log(myarr);
-myarr.pop(23)
-console.log(myarr);
-console.log(myarr.indexof(23));
+// myarr.push(545)
+// console.log(myarr);
+// myarr.pop(23)
+// console.log(myarr);
+// console.log(myarr.indexof(23));
 
-myarr = [32, 42, 54, 65, 65, 54]
-console.log(arr.length);
+// myarr = [32, 42, 54, 65, 65, 54]
+// console.log(arr.length);
 
-myarr = [234, 454, 546, 65, 6565]
-console.log(myarr.length);
+// myarr = [234, 454, 546, 65, 6565]
+// console.log(myarr.length);
 
-myarr = [34, 54, 65, 76, 765, 43]
-console.log(myarr.includes(34));
-console.log(myarr.includes(54));
-console.log(myarr.includes(53434));
+// myarr = [34, 54, 65, 76, 765, 43]
+// console.log(myarr.includes(34));
+// console.log(myarr.includes(54));
+// console.log(myarr.includes(53434));
 
-myarr = [34, 54, 65, 657, 123]
-let squared = myarr.map(x => x * x);
-myarr.forEach(x => console.log(x));
+// myarr = [34, 54, 65, 657, 123]
+// let squared = myarr.map(x => x * x);
+// myarr.forEach(x => console.log(x));
 
-let myarr1 = [4, 5]
-let myarr2 = [65, 65]
-let merged = arr.concat(myarr2)
-console.log(merged);
+// let myarr1 = [4, 5]
+// let myarr2 = [65, 65]
+// let merged = arr.concat(myarr2)
+// console.log(merged);
 
-myarr1 = [343, 434, 544]
-myarr2 = [3434, 54545, 656]
-console.log(...myarr1, ...myarr2);
+// myarr1 = [343, 434, 544]
+// myarr2 = [3434, 54545, 656]
+// console.log(...myarr1, ...myarr2);
 
-myarr1 = [234, 545, 654, 432]
-myarr.sort((a,b) => a - b);
+// myarr1 = [234, 545, 654, 432]
+// myarr.sort((a,b) => a - b);
 
-myarr1 = [434, 54, 546, 6555]
-myarr.sort((a, b) = a - b );
+// myarr1 = [434, 54, 546, 6555]
+// myarr.sort((a, b) = a - b );
 
-myarr = [34444444, 5, 656, 6776]
-let str = myarr.join(" , ");
-console.log(str);
-console.log(typeof str);
+// myarr = [34444444, 5, 656, 6776]
+// let str = myarr.join(" , ");
+// console.log(str);
+// console.log(typeof str);
 
-let myname = "deepak singh bhainsora"
-let arr = myname.join(" , ");
-console.log(arr);
-console.log(typeof arr);
+// let myname = "deepak singh bhainsora"
+// let arr = myname.join(" , ");
+// console.log(arr);
+// console.log(typeof arr);
 
-let arr1 = [323, 43, 4, 4, 6 , 44444, 5]
-let max = Math.max(...arr1);
-console.log(max);
+// let arr1 = [323, 43, 4, 4, 6 , 44444, 5]
+// let max = Math.max(...arr1);
+// console.log(max);
 
 
-let arr1 = [23, 4, 54, 1, 453, 1234]
-let min = Math.min(...arr1)
-console.log(min);
+// let arr1 = [23, 4, 54, 1, 453, 1234]
+// let min = Math.min(...arr1)
+// console.log(min);
 
-let arr1 = [23, 434, 54, 645]
-let arr2 = [34, 54, 634, 545]
-let arr3 = arr1 + arr2;
-let arr4 = [arr3]
-console.log(arr4);
+// let arr1 = [23, 434, 54, 645]
+// let arr2 = [34, 54, 634, 545]
+// let arr3 = arr1 + arr2;
+// let arr4 = [arr3]
+// console.log(arr4);
 
-let me = [35, 54, 56, 67]
-let my = [23, 32, 323, 323]
-let mn = [...me, ...my];
-console.log(mn);
-mn.push(34);
-console.log(mn);
+// let me = [35, 54, 56, 67]
+// let my = [23, 32, 323, 323]
+// let mn = [...me, ...my];
+// console.log(mn);
+// mn.push(34);
+// console.log(mn);
 
+// let myarrr = [23, 43, 54, 545]
+// console.log(myarrr.includes(9));
+
+// let m = ["deepak", "singh", "bhainsora"]
+// let b = ["I am from Haldwani"]
+// let cc = [...m, ...b]
+// console.log(cc);
+
+// let my = ["jkldsajflkdsjflk"]
+// console.log(Array.isArray(my));
+
+// let a = 34232;
+// let b = 34243;
+// let c = "hitesh";
+// console.log(Array.of(a, b, c));
+
+let a = [334, 344, 3535, 54]
+let b = a.indexOf(1);
+a.splice(indexOf(1));
+console.log(b);
 
 
