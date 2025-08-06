dda198f56e2d094632ceea6e339116e140cfdbf3
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-06 11:23:47 +0530

    Add files via upload

--- objects.js
+++ objects.js
@@ -1,32 +1,46 @@
-let a= {
-    name : "deepak",
-    fjkf : "fjkjfkdfkj",
-    age : 20,
-    location : "Haldwani"
-}
-console.log(a.name);
-console.log(a.fjkf);
-console.log(typeof a.name);
+// let a= {
+//     name : "deepak",
+//     fjkf : "fjkjfkdfkj",
+//     age : 20,
+//     location : "Haldwani"
+// }
+// console.log(a.name);
+// console.log(a.fjkf);
+// console.log(typeof a.name);
 
-a.name = "bhainsora"
-Object.freeze(a)
-a.name = "singh"
+// a.name = "bhainsora"
+// Object.freeze(a)
+// a.name = "singh"
 
-let introdution = {
-    name : "Deepak Singh Bhaisora",
-    location : "haldwani",
-    age : "20",
-    college : "Graphic Era Hill University",
-}
-console.log(introdution.name);
-console.log(typeof introdution.name);
+// let introdution = {
+//     name : "Deepak Singh Bhaisora",
+//     location : "haldwani",
+//     age : "20",
+//     college : "Graphic Era Hill University",
+// }
+// console.log(introdution.name);
+// console.log(typeof introdution.name);
 
-introdution.name = "singhbhainsora"
-console.log("introduction.name");
+// introdution.name = "singhbhainsora"
+// console.log("introduction.name");
 
-introdution.greeting = function(){
-    console.log("hello lkjdfsa");
+// introdution.greeting = function(){
+//     console.log("hello lkjdfsa");
     
-}
-console.log(introdution.greeting());
+// }
+// console.log(introdution.greeting());
 
+// let my={
+
+// }
+
+let data = {
+    id : "12133",
+    jfjk : "jfklsdf",
+    fullname : {
+        firstname : "deepak",
+        lastname : "bhainsora"
+    }
+}
+console.log(data.fullname.firstname);
+console.log(data.fullname());
