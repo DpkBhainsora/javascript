a070a0777bd106880b3b8a21d7181a28342cf7c0
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-08 12:12:18 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+// function addtwonumber(num1, number2 ){
+//     console.log(num1 + number2);
+//     console.log(num1 - number2);
+    
+// }
+// addtwonumber(3, 5)
+// addtwonumber(23, 88)
+
+// function kfj(number1, number2){
+//     let af = number1 + number2
+//     let fb = number1 - number2
+//     return af, fb
+    
+// }
+// let arr = kfj(23, 43)
+// console.log("Result:", arr);
+
+function calculatecratprice(...num1){
+    return num1
+}
+console.log(calculatecratprice(7, 23, 32));
