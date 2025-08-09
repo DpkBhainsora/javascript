047d449874ce8f5441ea22aabd89bb849b8d1cd4
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-09 20:41:24 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,41 @@
+// let a = 34;
+// var b = 34;
+// const c = 34;
+// if(true){
+//     let a = 20
+//     console.log("inner: a");
+    
+// }
+
+// console.log(a, b, c);
+
+
+// function one(){
+//     let user = "deepak"
+
+//     function two(){
+//         let website = "youtube"
+//         console.log(user);
+              
+
+//     }
+//    // console.log(website);
+    
+//     two()
+// }
+// one()
+
+// function add(num1, num2){
+//     add=  num1 + num2
+
+//     function sub(num4, num3){
+//         sub= num4 - num3
+//         console.log(add);
+//         sub(4, 8)
+//     }
+    
+
+// }
+// add();
+
+
