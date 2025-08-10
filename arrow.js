827c9eddf1e6c2d57a944edf850ac6e92a473ec0
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-10 10:27:48 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,15 @@
+// let user = {
+//     username: "depak",
+//     price: 233,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username}, welcome to webiste`);
+//         console.log(this);
+        
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "sjklf"
+// user.welcomeMessage()
+
+
