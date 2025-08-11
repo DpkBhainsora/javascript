26c0815d4343037e69ebaa19b5a90f7c83006a93
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-11 13:12:33 +0530

    Add files via upload

---
+++ CS.js
@@ -0,0 +1,24 @@
+// let temp = 34
+// if (temp > 50){
+//     console.log("less");
+    
+// }else
+// {
+//     console.log("more");
+    
+// }
+
+// console.log("temp is hot");
+
+
+// let balance = 1000
+// if(balance > 500){
+//     console.log("good");
+    
+// }else
+// {
+//     console.log("not good");
+    
+// }
+// console.log("good or bad");
+
