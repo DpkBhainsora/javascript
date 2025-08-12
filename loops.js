4115cc3f6417e4e5b85383738bef9e8cc4f6cafb
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-12 11:38:27 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 100; i++) {
+    console.log(`outer loop value : ${i}`);
+    for (let j = 0; j < 10; j++) {
+        const element = array[j];
+        
+    }
+    const element = array[i];
+    
+}
+
