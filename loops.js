8e4376dba42e121069b8a78eb63eb8ea1b6101e3
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-17 16:22:33 +0530

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,10 +1,75 @@
-for (let i = 0; i < 100; i++) {
-    console.log(`outer loop value : ${i}`);
-    for (let j = 0; j < 10; j++) {
-        const element = array[j];
+// for (let i = 0; i < 100; i++) {
+//     console.log(`outer loop value : ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         const element = array[j];
         
-    }
-    const element = array[i];
+//     }
+//     const element = array[i];
     
-}
+// }
+
+
+// let i = 0
+// while (i <= 10 ) {
+//     console.log(`value of index is ${i} `);
+//     i = i + 2
+    
+// }
+
+
+// let arr = [ "deepak", "singh", "bhainsora"]
+// let (i < arr.length) {
+//     console.log("value");
+//     i = i + 1
+// }
+
+// let score = 4
+// do{
+//     console.log("score");
+//     score++
+    
+// }
+// while(score <5);
+
+// let arr = [2, 4, 5, 5, 6]
+// for(let num of arr){
+//     console.log(num);
+    
+// }
+
+// let greet = "deepak"
+// for(let a of greet){
+//     console.log("each char");
+    
+// }
+
+// let myobject = {
+//     game: "deeepak",
+//     game1: "deepaksingh"
+// }
+// for (let key in myobject) {
+//     console.log(myobject[key]);
+    
+// }
+
+// let arr = ["deepak", "singh", "bhainsora"]
+// arr.forEach(function(val){
+//     console.log(val);
+    
+// })
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8]
+// let newnums = mynmums.filter( (nums) => {
+//     return num> 4 
+// })
+
+// (function(){
+//     console.log("hello" + name);
+    
+// });
+
+
+
+
+
 
