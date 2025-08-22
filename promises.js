c5ab1d670bb59a10282740d61199ba6a23a604ed
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-08-22 11:48:17 +0530

    Add files via upload

--- promises.js
+++ promises.js
@@ -22,3 +22,51 @@ Promiseone.then(function(){
     console.log("Promise 2 ");
     
 })
+
+let Promisethree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username:"Deepak",
+            email:"depak@gmail.com"
+        })
+    })
+})
+Promisethree.then(function(user){
+    console.log(user);
+    
+
+})
+ 
+let Promisefour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true 
+        if(!error){
+        resolve({username:"Deepak",
+            email:"depak@gmail.com"
+        })}
+        else{
+            reject('error')
+
+        }
+    },1000)
+    })
+
+async function consumePromisefour() {
+    try {
+        let response = await Promisefour
+        console.log(response);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+    
+    
+}
+consumePromisefour()
+
+
+
+
+
+
