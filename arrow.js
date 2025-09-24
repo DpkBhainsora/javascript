42393c17353ea59cff11a5193aced1b93f813dcc
Deepak Singh Bhaisora <deepakbhaisora017@gmail.com>
2025-09-24 12:34:40 +0530

    Add files via upload

--- arrow.js
+++ arrow.js
@@ -1,62 +1,62 @@
 
-// let user = {
-//     username: "depak",
-//     price: 233,
+let user = {
+    username: "depak",
+    price: 233,
 
-//     welcomeMessage: function(){
-//         console.log(`${this.username}, welcome to webiste`);
-//         console.log(this);
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to webiste`);
+        console.log(this);
         
-//     }
-// }
-// user.welcomeMessage()
-// user.username = "sjklf"
-// user.welcomeMessage()
+    }
+}
+user.welcomeMessage()
+user.username = "sjklf"
+user.welcomeMessage()
 
-// let aa = {
-//     username: "deepak",
-//     fullname: "deepak Singh Bhaisora",
+let aa = {
+    username: "deepak",
+    fullname: "deepak Singh Bhaisora",
 
-//     location : function (){
-//         let fromwhichstate = "haldwani"
-//         console.log(fromwhichstate);
-//     }
+    location : function (){
+        let fromwhichstate = "haldwani"
+        console.log(fromwhichstate);
+    }
     
-// }
-// console.log(aa);
-// aa.location();
-// aa.username = "anupam"
-// console.log(aa);
+}
+console.log(aa);
+aa.location();
+aa.username = "anupam"
+console.log(aa);
 
-// let a=(1,2,3,4,4,43,4)
-// console.log(a);
+let a=(1,2,3,4,4,43,4)
+console.log(a);
 
 
-// function add(){
-//     let usrname = "deepaK"
+function add(){
+    let usrname = "deepaK"
     
-// }
+}
 
-// console.log(add)
+console.log(add)
 
-// let addtwo = (num1, num2) => (num1 + num2) 
-// console.log(addtwo(3, 4))
+let addtwo = (num1, num2) => (num1 + num2) 
+console.log(addtwo(3, 4))
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// let arr1 = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-// let add1 = [...arr, ...arr1]
-// console.log(add1);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let arr1 = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+let add1 = [...arr, ...arr1]
+console.log(add1);
 
-// a= "deepak"
-// b= "singh"
-// c= "bhainsora"
-// add1= [...a, ...b, ...c]
-// console.log(add1);
+a= "deepak"
+b= "singh"
+c= "bhainsora"
+add1= [...a, ...b, ...c]
+console.log(add1);
 
         
-// let arr = [1, 2, "deepak", 3, 5];
-// let ab = arr.reverse;
-// console.log(ab);
+let arr = [1, 2, "deepak", 3, 5];
+let ab = arr.reverse;
+console.log(ab);
 
 
 
